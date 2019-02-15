08e8acbbc79d3f0ccf66eb1e6d715f9076f65f2c
shervin <shervin.ha@gmail.com>
2019-02-15 23:39:51 +0330

    class

--- webapp/models.py
+++ webapp/models.py
@@ -3,6 +3,8 @@ from django.db import models
 # Create your models here.
 class Exchanges(models.Model):
     exchange_name = models.CharField(max_length = 20)
-
+    price = models.DecimalField(max_digits=7, decimal_places=2)
+    volume = models.DecimalField(max_digits=7, decimal_places=3)
+    #date = models.DateTimeField(auto_now_add=True)
     def __str__(self):
         return "NAME_IS : {}".format(self.exchange_name)
