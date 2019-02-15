7c12ebf6ba69498c0b7c01ef42400331678d208f
shervin <shervin.ha@gmail.com>
2019-02-15 17:46:17 +0330

    class

--- webapp/models.py
+++ webapp/models.py
@@ -3,4 +3,6 @@ from django.db import models
 # Create your models here.
 class Exchanges(models.Model):
     exchange_name = models.CharField(max_length = 20)
-    #exchange_price = models.CharField(max_length = 20)
+
+    def __str__(self):
+        return "NAME_IS : {}".format(self.exchange_name)
