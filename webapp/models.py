0d8f992b9d7b3d493cbf05fefaef312e3c8afedf
shervin <shervin.ha@gmail.com>
2019-02-15 17:12:55 +0330

    class

--- webapp/models.py
+++ webapp/models.py
@@ -3,4 +3,4 @@ from django.db import models
 # Create your models here.
 class Exchanges(models.Model):
     exchange_name = models.CharField(max_length = 20)
-    exchange_price = models.BigIntegerField()
+    #exchange_price = models.CharField(max_length = 20)
