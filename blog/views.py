891a8ba9fe4434e9c577627d1a2214b96c364394
shervin <shervin.ha@gmail.com>
2019-02-16 21:44:36 +0330

    "ok"

--- blog/views.py
+++ blog/views.py
@@ -3,4 +3,5 @@ from django.http import HttpResponse
 # Create your views here.
 
 def home(request):
-    return HttpResponse('<h1>THIS IS MY BLOG</h1>')
+    #return HttpResponse('<h1>THIS IS SHERVIN BLOG</h1>')
+    return render(request, "blog/home.html")
